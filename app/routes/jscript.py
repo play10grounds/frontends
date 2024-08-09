ad821a35936bca0133a65d81b0e4e8a3eaa9ead7
play10grounds <play10grounds@hotmail.com>
2024-08-09 17:56:54 +0900

    dom, event, ajax를 이용한 우편번호 검색

--- app/routes/jscript.py
+++ app/routes/jscript.py
@@ -9,15 +9,15 @@ templates = Jinja2Templates(directory='views/templates')
 
 # 라우트 설정
 @jscript_router.get('/')
-async def flex(req: Request):
+async def index(req: Request):
     return templates.TemplateResponse('js/index.html', {'request': req})
 
 @jscript_router.get('/hello')
-async def flex(req: Request):
+async def hello(req: Request):
     return templates.TemplateResponse('js/01hello.html', {'request': req})
 
 @jscript_router.get('/type')
-async def flex(req: Request):
+async def type(req: Request):
     return templates.TemplateResponse('js/02type.html', {'request': req})
 
 @jscript_router.get('/operator')
@@ -25,49 +25,53 @@ async def flex(req: Request):
     return templates.TemplateResponse('js/03operator.html', {'request': req})
 
 @jscript_router.get('/condition')
-async def flex(req: Request):
+async def condition(req: Request):
     return templates.TemplateResponse('js/04condition.html', {'request': req})
 
 @jscript_router.get('/loop')
-async def flex(req: Request):
+async def loop(req: Request):
     return templates.TemplateResponse('js/05loop.html', {'request': req})
 
 @jscript_router.get('/array')
-async def flex(req: Request):
+async def array(req: Request):
     return templates.TemplateResponse('js/06array.html', {'request': req})
 
 @jscript_router.get('/while')
-async def flex(req: Request):
+async def whiles(req: Request):
     return templates.TemplateResponse('js/07while.html', {'request': req})
 
 @jscript_router.get('/function')
-async def flex(req: Request):
+async def function(req: Request):
     return templates.TemplateResponse('js/08function.html', {'request': req})
 
 @jscript_router.get('/callback')
-async def flex(req: Request):
+async def callback(req: Request):
     return templates.TemplateResponse('js/09callback.html', {'request': req})
 
 @jscript_router.get('/except')
-async def flex(req: Request):
+async def excepts(req: Request):
     return templates.TemplateResponse('js/10except.html', {'request': req})
 
 @jscript_router.get('/bom')
-async def flex(req: Request):
+async def bom(req: Request):
     return templates.TemplateResponse('js/11bom.html', {'request': req})
 
 @jscript_router.get('/dom')
-async def flex(req: Request):
+async def dom(req: Request):
     return templates.TemplateResponse('js/12dom.html', {'request': req})
 
 @jscript_router.get('/event')
-async def flex(req: Request):
+async def event(req: Request):
     return templates.TemplateResponse('js/13event.html', {'request': req})
 
 @jscript_router.get('/form')
-async def flex(req: Request):
+async def form(req: Request):
     return templates.TemplateResponse('js/14form.html', {'request': req})
 
 @jscript_router.get('/ajax')
-async def flex(req: Request):
+async def ajax(req: Request):
     return templates.TemplateResponse('js/15ajax.html', {'request': req})
+
+@jscript_router.get('/zipcode')
+async def zipcode(req: Request):
+    return templates.TemplateResponse('js/16zipcode.html', {'request': req})
